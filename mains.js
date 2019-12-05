773f1353322730cd72fc2e27651d370c667faba4
Yomna <yomna@netc.fr>
2019-12-05 14:36:56 +0100

    Update mains.js

--- mains.js
+++ mains.js
@@ -4,18 +4,15 @@ const {
 } = require('electron')
 
 
-// Gardez une reference globale de l'objet window, si vous ne le faites pas, la fenetre sera
-// fermee automatiquement quand l'objet JavaScript sera garbage collected.
 let win
 
 function createWindow() {
-  // Créer le browser window.
   win = new BrowserWindow({
     webPreferences: {
       nodeIntegration: true
     },
-    // width: 852,
-    width: 1252,
+    width: 852,
+    // width: 1252,
     height: 700,
     frame: true,
     resizable: false,
@@ -26,31 +23,23 @@ function createWindow() {
     //transparent: true
   })
 
-  // and load the index.html of the app.
   win.loadFile('index.html')
 
-  // Ouvre les DevTools.
-  win.webContents.openDevTools()
+  // win.webContents.openDevTools()
 
   win.setMenu(null);
 
-  // Émit lorsque la fenêtre est fermée.
   win.on('closed', () => {
-    // Dé-référence l'objet window , normalement, vous stockeriez les fenêtres
-    // dans un tableau si votre application supporte le multi-fenêtre. C'est le moment
-    // où vous devez supprimer l'élément correspondant.
     win = null
   })
 }
 
 
 
-// Cette méthode sera appelée quant Electron aura fini
-// de s'initialiser et sera prêt à créer des fenêtres de navigation.
-// Certaines APIs peuvent être utilisées uniquement quand cet événement est émit.
+
 app.on('ready', createWindow)
 
-// Quitte l'application quand toutes les fenêtres sont fermées.
+
 app.on('window-all-closed', () => {
   app.quit()
 })
